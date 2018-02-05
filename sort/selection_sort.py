b9cdc933474202666fb72ec47cca98285374164b
Albino1995 <763968024@qq.com>
2018-02-05 09:15:17 +0800

    选择排序

---
+++ sort/selection_sort.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'Albino'
+
+
+def selection_sort(list):
+    """
+    选择排序
+    """
+    for i in range(len(list)):
+        # 寻找[i, n)区间的最小值
+        min_index = i
+        for j in range(i+1, len(list)):
+            if list[j] < list[min_index]:
+                min_index = j
+        # 交换
+        list[i], list[min_index] = list[min_index], list[i]
+    return list
