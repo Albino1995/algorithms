46d6d520578ed2e14a4474735903e6e28341e9cc
Albino1995 <763968024@qq.com>
2018-02-05 09:15:06 +0800

    插入排序

---
+++ sort/insert_sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'Albino'
+
+
+def insert_sort(list):
+    """
+    插入排序
+    """
+    for i in range(1, len(list)):
+        # 寻找list[i]合适的插入位置, 第一个元素默认排好序
+        j = i
+        while j > 0:
+            if list[j] < list[j-1]:
+                list[j-1], list[j] = list[j], list[j-1]
+            else:
+                break
+            j -= 1
+
+    return list
