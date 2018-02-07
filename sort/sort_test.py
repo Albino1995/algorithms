00e732b92ff06d266797d41ad0d95389288ef907
Albino1995 <763968024@qq.com>
2018-02-07 09:32:14 +0800

    test

--- sort/sort_test.py
+++ sort/sort_test.py
@@ -2,16 +2,18 @@
 __author__ = 'Albino'
 
 import random
-import time
 
-from sort.insert_sort import insert_sort
+from sort.insertion_sort import insertion_sort
 from sort.selection_sort import selection_sort
+from sort.bubble_sort import bubble_sort
 
 
 def generate_list(n, l, r):
     return [random.randint(l, r) for x in range(n)]
 
+
 if __name__ == '__main__':
     test_list = generate_list(1000, 1, 1000)
     # selection_sort(test_list)
-    insert_sort(test_list)
+    # insertion_sort(test_list)
+    bubble_sort(test_list)
