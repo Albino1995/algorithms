daa3e56a28093a943bc894b74484ae1bd1596d0e
Albino1995 <763968024@qq.com>
2018-02-07 09:32:04 +0800

    插入排序

---
+++ sort/insertion_sort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = 'Albino'
+
+from .clock import computed_time
+
+
+@computed_time
+def insertion_sort(list):
+    """
+    插入排序
+    """
+    for i in range(1, len(list)):
+        # 待插入的元素
+        e = list[i]
+        # e应该插入的位置
+        j = i
+        while j > 0 and list[j-1] > e:
+            list[j] = list[j-1]
+            j -= 1
+        list[j] = e
+
+    return list
