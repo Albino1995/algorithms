48d49c4e71b59b1edf58928fec08a4fa8bcd4be5
Albino1995 <763968024@qq.com>
2018-02-07 11:13:33 +0800

    计时装饰器

--- sort/clock.py
+++ sort/clock.py
@@ -4,9 +4,16 @@ __author__ = 'Albino'
 import time
 
 
-def computed_time(func):
-    def wrapper(*args):
-        time.clock()
-        func(*args)
-        print(time.clock())
+def computed_time(func, repeat=set()):
+    def wrapper(*args, **kwargs):
+        if func not in repeat:
+            repeat.add(func)
+            t0 = time.clock()
+            result = func(*args, **kwargs)
+            print(time.clock() - t0)
+            # 拦截递归调用的函数后清空set
+            repeat.remove(func)
+        else:
+            result = func(*args, **kwargs)
+        return result
     return wrapper
