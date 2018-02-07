335fa3831e2c858d95bbf81055a44f87c98678a4
Albino1995 <763968024@qq.com>
2018-02-07 09:31:38 +0800

    冒泡排序

---
+++ sort/bubble_sort.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+__author__ = 'Albino'
+
+from .clock import computed_time
+
+
+@computed_time
+def bubble_sort(list):
+    for i in range(len(list) - 1):
+        # 内层循环中后i个数已经有序
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
