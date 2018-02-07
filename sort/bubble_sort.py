a13dc5f5d70ee0260f3916d5db4074eb5dc16ee8
Albino1995 <763968024@qq.com>
2018-02-07 11:13:39 +0800

    冒泡排序

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 __author__ = 'Albino'
 
-from .clock import computed_time
+from sort.clock import computed_time
 
 
 @computed_time
-def bubble_sort(list):
+def BubbleSort(list):
     for i in range(len(list) - 1):
         # 内层循环中后i个数已经有序
         for j in range(len(list) - 1 - i):
