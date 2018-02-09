6d52d086e6af5d513dee4c7f73c505f0b185227f
Albino1995 <763968024@qq.com>
2018-02-09 14:38:17 +0800

    排序测试

--- sort/sort_test.py
+++ sort/sort_test.py
@@ -7,6 +7,7 @@ from sort.insertion_sort import InsertionSort
 from sort.selection_sort import SelectionSort
 from sort.bubble_sort import BubbleSort
 from sort.merge_sort import MergeSort
+from sort.quick_sort import QuickSort
 
 
 def generate_list(n, l, r):
@@ -14,8 +15,9 @@ def generate_list(n, l, r):
 
 
 if __name__ == '__main__':
-    test_list = generate_list(1000, 1, 1000)
+    test_list = generate_list(5000, 1, 5000)
     # SelectionSort(test_list)
     # InsertionSort(test_list)
     # BubbleSort(test_list)
-    MergeSort(test_list)
+    # MergeSort(test_list)
+    QuickSort(test_list, 0, len(test_list) - 1)
