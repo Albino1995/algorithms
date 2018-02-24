334f533c6934757067a213188b84300b7fcced8b
Albino1995 <763968024@qq.com>
2018-02-24 16:09:04 +0800

    排序测试

--- sort/sort_test.py
+++ sort/sort_test.py
@@ -7,8 +7,9 @@ from sort.insertion_sort import InsertionSort
 from sort.selection_sort import SelectionSort
 from sort.bubble_sort import BubbleSort
 from sort.merge_sort import MergeSort
-# from sort.quick_sort import QuickSort
+from sort.quick_sort import QuickSort
 from sort.quick_sort_3_ways import QuickSort3Ways
+from sort.heap_sort import HeapSort
 
 
 def generate_list(n, l, r):
@@ -16,9 +17,11 @@ def generate_list(n, l, r):
 
 
 if __name__ == '__main__':
-    test_list = generate_list(1000, 1, 1000)
+    test_list = generate_list(5000, 1, 5000)
     # SelectionSort(test_list)
     # InsertionSort(test_list)
     # BubbleSort(test_list)
     # MergeSort(test_list)
-    QuickSort3Ways(test_list, 0, len(test_list) - 1)
+    # QuickSort(test_list, 0, len(test_list) - 1)
+    # QuickSort3Ways(test_list, 0, len(test_list) - 1)
+    HeapSort(test_list)
